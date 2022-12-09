00daf256c40406776abf37be60308274e99fba26
Christian Kröger <christian.kroeger@fincon.eu>
2022-12-09 18:14:02 +0100

    initial commit

---
+++ src/main/java/com/github/ckroeger/javajs/JavaJsApplication.java
@@ -0,0 +1,13 @@
+package com.github.ckroeger.javajs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJsApplication.class, args);
+	}
+
+}
